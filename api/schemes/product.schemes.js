91129b4a6c5645debd768bac8465f1654b064d69
Brian Lara <brian.lara@outlook.com.ar>
2022-11-29 16:03:52 -0300

    yup

--- api/schemes/product.schemes.js
+++ api/schemes/product.schemes.js
@@ -1,10 +1,10 @@
-import yup from 'yup';
+import yup from 'yup'
 
 const productScheme = yup.object({
     name: yup.string().required('El nombre es obligatorio'),
     price: yup.number().positive('El precio debe ser positivo').required('El precio es obligatorio'),
     category: yup.string().required('La categoría es obligatoria'),
-}).noUnknown();
+}).noUnknown()
 
 
 export {
